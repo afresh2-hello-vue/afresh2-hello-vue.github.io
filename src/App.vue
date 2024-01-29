680760814a2eb4df6f38d87eb1f8d32ed76524c8
afresh2 <afresh320@daum.net>
2024-01-30 06:22:50 +0900

    var

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,8 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-
-// reactive state(초기값 선언 : 0)
-const count = ref({
-	cnt:0,
-	name: "KIM",
-})
-
-// functions that mutate state and trigger updates
-// 화면에서 버튼 클릭할 때마다 count 1씩 증가
-function increment() {
-	count.value.cnt ++
-	count.value.name = count.value.name.split('').reverse().join('')
-}
-
-// lifecycle hooks
-onMounted(() => {
-	console.log(`The initial count is ${count.value}.`)
-})
+import { ref } from 'vue'
+const message = ref({name:"MIN", age:46})
 </script>
 
 <template>
-	<button @click="increment">Count is: {{ count.cnt }} {{  count.name }}</button>
+	<h1>{{ message }}</h1>
 </template>
-
-<style scoped>
-button {
-	font-weight: bold;
-	}
-</style>

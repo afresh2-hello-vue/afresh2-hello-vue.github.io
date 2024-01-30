0343e6f2168e577a58e25162b92e28c62bae0d58
afresh2 <afresh320@daum.net>
2024-01-30 21:31:25 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,38 @@
 <script setup>
 import { ref } from 'vue'
 
-let id = 0
+// 상태 변수 설정
+const show = ref(true)
+const list = ref([1, 2, 3])
 
-const newTodo = ref('')
-const todos = ref([
-	{ id: id++, issue: 'Backlog' } , { id: id++, issue: 'Todo'},
-	{ id: id++, issue: 'Going Hawaii'}
-])
-
-function addTodo(/*e*/){
-	//e.preventDefault();
-	todos.value.push({id: id++, issue: newTodo.value})	
+// 함수
+const addItemToList = () => {
+	list.value.push(list.value.length + 1)
 }
 
-
-function removeTodo(todo){
-	todos.value.splice(todo.index, 1)
-	//todos remove ...[].filter(t/f)
-	//todos.value = todos.value.filter((t) => t !== ?)
+const removeItemFromList = () => {
+	list.value.pop()
 }
 
+const reverseListOrder = () => {
+	list.value.reverse()
+}
 </script>
 
 <template>
-	<form @submit.prevent ="addTodo">
-		<input v-model = "newTodo">
-		<button>할 일 추가</button>
-	</form>
-	<ul>
-		<li v-for = "todo in todos">
-			{{ todo.issue }} - <button @click = "removeTodo(todo)" > Done </button>	
-		</li>
+	<button @click = "show = !show">List렌더링 ON/OFF</button>
+        <button @click = "addItemToList">List추가</button>
+        <button @click = "removeItemFromList">List제거</button>
+        <button @click = "reverseListOrder">List뒤집기</button>
+
+	<!-- List를 보여주는 부분 -->
+	<ul v-if="show">
+		<li v-for="item of list">{{ item }}</li>
 	</ul>
+	
+	<!-- List가 비어있지 않으면서 숨겨져 있는 경우 -->
+	<p v-else-if="list.length">List is not empty, but hidden.</p>
+
+	<!-- List가 비어 있을 경우 -->
+	<p v-else>List is empty.</p>
 </template>
-
-
-

bcc19eddfff14eb643ef62d2e7273d4c99842b5c
afresh2 <afresh320@daum.net>
2024-01-30 18:23:33 +0900

    다양한 이벤트에 대한 로그 출력

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { ref, onUpdated } from 'vue'
+import { ref, onUpdated, onMounted, onBeforeUpdate } from 'vue'
 
 const count = ref(0)
+
+onMounted(() => {
+	console.log("Component mounted")
+})
+
+onBeforeUpdate(() => {
+	console.log("Component is about to update")
+})
+
 onUpdated(() => { 
 	console.log(document.getElementById('count').textContent)})
 </script>
+
 
 <template>
         <button id = "count" @click = "count++"> {{ count }} </button>

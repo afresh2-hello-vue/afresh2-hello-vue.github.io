cba634d953c561358f35de186106a4da23ad8a15
afresh2 <afresh320@daum.net>
2024-01-30 18:17:41 +0900

    count button 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onUpdated } from 'vue'
 
-const message = ref('')
+const count = ref(0)
+onUpdated(() => { 
+	console.log(document.getElementById('count').textContent)})
 </script>
 
 <template>
-        <span>Multi-line message is: </span>
-	<p style="white-space: pre-line;"> {{ message }} </p>
-	<textarea v-model = "message" placeholder = "add multiple lines" ></textarea>
+        <button id = "count" @click = "count++"> {{ count }} </button>
 </template>
+
